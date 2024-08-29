ecce9ecbe93106b36b37282e5c842bfd30b6738c
BeCK8Pi <khuntip1207@gmail.com>
2024-08-29 18:56:58 +0700

    finishmuk

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -4,25 +4,70 @@ import { useContext } from "react";
 
 export default function Controller() {
   const { toggleTheme, setDarkTheme, setLightTheme } = useContext(ThemeContext);
-  const { toggleLang } = useContext(LangContext);
+  const { lang, toggleLang } = useContext(LangContext);
   return (
     <div className="d-flex justify-content-center gap-2">
       <button className="btn btn-secondary" onClick={setLightTheme}>
         {/* Please update this text so it shows in EN or TH */}
-        Light
+        {lang.buttons.setLightTheme}
       </button>
       <button className="btn btn-dark" onClick={setDarkTheme}>
         {/* Please update this text so it shows in EN or TH */}
-        Light Dark
+        {lang.buttons.setDarkTheme}
       </button>
       <button className="btn btn-primary" onClick={toggleTheme}>
         {/* Please update this text so it shows in EN or TH */}
-        Light Toggle Theme
+        {lang.buttons.toggleTheme}
       </button>
       <button className="btn btn-danger" onClick={toggleLang}>
         {/* Please update this text so it shows in EN or TH */}
-        Light Toggle Language
+        {lang.buttons.toggleLang}
       </button>
     </div>
   );
 }
+
+/* 
+const THEMES = {
+    LIGHT: {
+      name: "light",
+      bgColor: "white",
+      fgColor: "black",
+    },
+    DARK: {
+      name: "dark",
+      bgColor: "black",
+      fgColor: "white",
+    },
+  };
+
+const LANGS = {
+    EN: {
+      name: "en",
+      header: "A simple useContext Example",
+      detail: {
+        lightActivated: "light theme activated",
+        darkActivated: "dark theme activated",
+      },
+      buttons: {
+        setLightTheme: "Light",
+        setDarkTheme: "Dark",
+        toggleTheme: "Toggle Theme",
+        toggleLang: "Toggle Language",
+      },
+    },
+    TH: {
+      name: "th",
+      header: "ตัวอย่างเว็บไซท์ useContext อย่างง่าย",
+      detail: {
+        lightActivated: "กำลังใช้ธีมไลท์",
+        darkActivated: "กำลังใช้ธีมดาร์ค",
+      },
+      buttons: {
+        setLightTheme: "ธีมไลท์",
+        setDarkTheme: "ธีมดาร์ค",
+        toggleTheme: "สลับธีม",
+        toggleLang: "สลับภาษา",
+      },
+    },
+  }; */
--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,15 +1,63 @@
 import { ThemeContext } from "@contexts/ThemeContext";
+import { LangContext } from "@contexts/LangContext";
 import { useContext } from "react";
 
 export const Detail = () => {
   const { theme } = useContext(ThemeContext);
+  const { lang } = useContext(LangContext);
   return (
     <div>
       <p className="text-center" style={{ color: theme.fgColor }}>
-        Display activated theme message here (EN or TH)
         {/* tip: Get information from "theme" variable to determine 
     which theme is currently selected */}
+        {theme.name==="light"&&lang.detail.lightActivated}
+        {theme.name==="dark"&&lang.detail.darkActivated}
       </p>
     </div>
   );
 };
+
+/* 
+const THEMES = {
+    LIGHT: {
+      name: "light",
+      bgColor: "white",
+      fgColor: "black",
+    },
+    DARK: {
+      name: "dark",
+      bgColor: "black",
+      fgColor: "white",
+    },
+  };
+
+const LANGS = {
+    EN: {
+      name: "en",
+      header: "A simple useContext Example",
+      detail: {
+        lightActivated: "light theme activated",
+        darkActivated: "dark theme activated",
+      },
+      buttons: {
+        setLightTheme: "Light",
+        setDarkTheme: "Dark",
+        toggleTheme: "Toggle Theme",
+        toggleLang: "Toggle Language",
+      },
+    },
+    TH: {
+      name: "th",
+      header: "ตัวอย่างเว็บไซท์ useContext อย่างง่าย",
+      detail: {
+        lightActivated: "กำลังใช้ธีมไลท์",
+        darkActivated: "กำลังใช้ธีมดาร์ค",
+      },
+      buttons: {
+        setLightTheme: "ธีมไลท์",
+        setDarkTheme: "ธีมดาร์ค",
+        toggleTheme: "สลับธีม",
+        toggleLang: "สลับภาษา",
+      },
+    },
+  }; */
